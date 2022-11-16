8b5e85552a80d95d52ad8b44c3e38b9947e5eca9
Omer Faruk Demir <omerfarukde@gmail.com>
2022-11-16 22:27:07 +0300

    Add postgresql with ktorm example

--- README.md
+++ README.md
@@ -133,6 +133,95 @@ fun Application.redis() {
 }
 ```
 
+- Custom Storage (postgresql with ktorm query dsl)
+
+```kotlin
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::postgreSQL).start(wait = true)
+}
+
+object FixedWindowTable : Table<Nothing>("fixed_window") {
+    val id = varchar("id").primaryKey()
+    val startInSeconds = int("start_in_seconds")
+    val requestCount = int("request_count")
+}
+
+class PostgreSQLFixedWindowStorage : FixedWindowStorage {
+
+    // make sure schema is ready bofore run this example
+    // CREATE DATABASE ktorate;
+    // CREATE TABLE fixed_window (id varchar NOT NULL PRIMARY KEY, start_in_seconds INT NOT NULL, request_count INT NOT NULL);
+
+    private val database = Database.connect(
+        url = "jdbc:postgresql://localhost:5432/ktorate",
+        user = "postgres",
+        password = "postgres"
+    )
+
+    override fun get(id: String): FixedWindowModel? {
+        return database.from(FixedWindowTable)
+            .select()
+            .where(FixedWindowTable.id eq id)
+            .map { it.fixedWindowModel() }
+            .firstOrNull()
+    }
+
+    override fun upsert(model: FixedWindowModel): FixedWindowModel {
+        return model.also {
+            database.insertOrUpdate(FixedWindowTable) {
+                set(it.id, model.id)
+                set(it.startInSeconds, model.startInSeconds)
+                set(it.requestCount, model.requestCount)
+                onConflict {
+                    set(it.startInSeconds, model.startInSeconds)
+                    set(it.requestCount, model.requestCount)
+                }
+            }
+        }
+    }
+
+    override fun all(): Collection<FixedWindowModel> {
+        return database.from(FixedWindowTable)
+            .select()
+            .map { it.fixedWindowModel() }
+    }
+
+    override fun delete(id: String): Boolean {
+        return database.delete(FixedWindowTable) { it.id eq id } == 1
+    }
+
+    override fun delete(ids: Collection<String>): Int {
+        return database.delete(FixedWindowTable) { it.id inList ids }
+    }
+
+    private fun QueryRowSet.fixedWindowModel(): FixedWindowModel {
+        return FixedWindowModel(
+            this[FixedWindowTable.id]!!,
+            this[FixedWindowTable.startInSeconds]!!,
+            this[FixedWindowTable.requestCount]!!
+        )
+    }
+}
+
+fun Application.postgreSQL() {
+    install(ktorate, configure = {
+        duration = 3.seconds
+        limit = 5
+        deleteExpiredRecordsPeriod = 5.seconds
+        synchronizedReadWrite = true
+        rateLimiter = FixedWindow(
+            duration = duration,
+            limit = limit,
+            synchronizedReadWrite = synchronizedReadWrite,
+            storage = PostgreSQLFixedWindowStorage()
+        )
+    })
+
+    routing { get("/") { call.respondText("Evet") } }
+}
+
+```
+
 ### TODO's
 
 - Fix confusion between Configuration and RateLimiters
@@ -145,3 +234,4 @@ fun Application.redis() {
 - CI (github actions?)
 - Token Bucket and Leaky Bucket strategies
 - Customizable limiter by route and HTTP method
+- Move external storage examples to source with its of packages
---
+++ src/test/kotlin/io/github/omerfarukdemir/ktorate/TestPostgreSQLApplication.kt
@@ -0,0 +1,101 @@
+package io.github.omerfarukdemir.ktorate
+
+import io.github.omerfarukdemir.ktorate.limiters.FixedWindow
+import io.github.omerfarukdemir.ktorate.models.FixedWindowModel
+import io.github.omerfarukdemir.ktorate.storages.FixedWindowStorage
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import org.ktorm.database.Database
+import org.ktorm.dsl.*
+import org.ktorm.schema.Table
+import org.ktorm.schema.int
+import org.ktorm.schema.varchar
+import org.ktorm.support.postgresql.insertOrUpdate
+import kotlin.time.Duration.Companion.seconds
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::postgreSQL).start(wait = true)
+}
+
+object FixedWindowTable : Table<Nothing>("fixed_window") {
+    val id = varchar("id").primaryKey()
+    val startInSeconds = int("start_in_seconds")
+    val requestCount = int("request_count")
+}
+
+class PostgreSQLFixedWindowStorage : FixedWindowStorage {
+
+    // make sure schema is ready bofore run this example
+    // CREATE DATABASE ktorate;
+    // CREATE TABLE fixed_window (id varchar NOT NULL PRIMARY KEY, start_in_seconds INT NOT NULL, request_count INT NOT NULL);
+
+    private val database = Database.connect(
+        url = "jdbc:postgresql://localhost:5432/ktorate",
+        user = "postgres",
+        password = "postgres"
+    )
+
+    override fun get(id: String): FixedWindowModel? {
+        return database.from(FixedWindowTable)
+            .select()
+            .where(FixedWindowTable.id eq id)
+            .map { it.fixedWindowModel() }
+            .firstOrNull()
+    }
+
+    override fun upsert(model: FixedWindowModel): FixedWindowModel {
+        return model.also {
+            database.insertOrUpdate(FixedWindowTable) {
+                set(it.id, model.id)
+                set(it.startInSeconds, model.startInSeconds)
+                set(it.requestCount, model.requestCount)
+                onConflict {
+                    set(it.startInSeconds, model.startInSeconds)
+                    set(it.requestCount, model.requestCount)
+                }
+            }
+        }
+    }
+
+    override fun all(): Collection<FixedWindowModel> {
+        return database.from(FixedWindowTable)
+            .select()
+            .map { it.fixedWindowModel() }
+    }
+
+    override fun delete(id: String): Boolean {
+        return database.delete(FixedWindowTable) { it.id eq id } == 1
+    }
+
+    override fun delete(ids: Collection<String>): Int {
+        return database.delete(FixedWindowTable) { it.id inList ids }
+    }
+
+    private fun QueryRowSet.fixedWindowModel(): FixedWindowModel {
+        return FixedWindowModel(
+            this[FixedWindowTable.id]!!,
+            this[FixedWindowTable.startInSeconds]!!,
+            this[FixedWindowTable.requestCount]!!
+        )
+    }
+}
+
+fun Application.postgreSQL() {
+    install(ktorate, configure = {
+        duration = 3.seconds
+        limit = 5
+        deleteExpiredRecordsPeriod = 5.seconds
+        synchronizedReadWrite = true
+        rateLimiter = FixedWindow(
+            duration = duration,
+            limit = limit,
+            synchronizedReadWrite = synchronizedReadWrite,
+            storage = PostgreSQLFixedWindowStorage()
+        )
+    })
+
+    routing { get("/") { call.respondText("Evet") } }
+}
