3563be4287ce5872f186ef46ed0ce62d460e8b04
Omer Faruk Demir <omerfarukde@gmail.com>
2022-11-23 20:46:36 +0300

    Who cares about the magic numbers

--- src/main/kotlin/io/github/omerfarukdemir/ktorate/utils/Now.kt
+++ src/main/kotlin/io/github/omerfarukdemir/ktorate/utils/Now.kt
@@ -1,9 +1,7 @@
 package io.github.omerfarukdemir.ktorate.utils
 
 object Now {
-    private const val millisInSecond = 1000
-
     fun seconds(): Int {
-        return (System.currentTimeMillis() / millisInSecond).toInt()
+        return (System.currentTimeMillis() / 1000).toInt()
     }
 }
